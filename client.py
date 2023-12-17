145e9e10674f2e3cbc2d9bcab0a9153326691742
Your Name <you@example.com>
2023-12-17 15:31:21 +0300

    first commit

--- client.py
+++ client.py
@@ -1,12 +1,93 @@
-# an object of WSGI application 
-from flask import Flask	 
-app = Flask(__name__) # Flask constructor 
+import tkinter as tk
+from tkinter import messagebox
+import socket
+import json
 
-# A decorator used to tell the application 
-# which URL is associated function 
-@app.route('/')	 
-def hello(): 
-	return 'HELLO'
+def send_request(action, username, password):
+    request = {"action": action, "username": username, "password": password}
+    return json.dumps(request).encode("utf-8")
 
-if __name__=='__main__': 
-    app.run() 
+def create_account():
+    try:
+        server_ip = entry_ip.get()
+        server_port = int(entry_port.get())
+        username = entry_username.get()
+        password = entry_password.get()
+
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((server_ip, server_port))
+
+        action = "create_account"
+        request_data = send_request(action, username, password)
+        client_socket.send(request_data)
+
+        response_data = client_socket.recv(1024).decode("utf-8")
+        response = json.loads(response_data)
+
+        messagebox.showinfo("Server Response", response['status'])
+
+    except socket.error as e:
+        messagebox.showerror("Connection Error", "No server available")
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred: {e}")
+
+    finally:
+        client_socket.close()
+
+def login():
+    try:
+        server_ip = entry_ip.get()
+        server_port = int(entry_port.get())
+        username = entry_username.get()
+        password = entry_password.get()
+
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((server_ip, server_port))
+
+        action = "login"
+        request_data = send_request(action, username, password)
+        client_socket.send(request_data)
+
+        response_data = client_socket.recv(1024).decode("utf-8")
+        response = json.loads(response_data)
+
+        messagebox.showinfo("Server Response", response['status'])
+
+    except socket.error as e:
+        messagebox.showerror("Connection Error", "No server available")
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred: {e}")
+
+    finally:
+        client_socket.close()
+
+# GUI setup
+root = tk.Tk()
+root.title("Client Interface")
+
+frame = tk.Frame(root)
+frame.pack(padx=20, pady=20)
+
+tk.Label(frame, text="Server IP:").grid(row=0, column=0, padx=5, pady=5)
+entry_ip = tk.Entry(frame)
+entry_ip.grid(row=0, column=1, padx=5, pady=5)
+
+tk.Label(frame, text="Server Port:").grid(row=1, column=0, padx=5, pady=5)
+entry_port = tk.Entry(frame)
+entry_port.grid(row=1, column=1, padx=5, pady=5)
+
+tk.Label(frame, text="Username:").grid(row=2, column=0, padx=5, pady=5)
+entry_username = tk.Entry(frame)
+entry_username.grid(row=2, column=1, padx=5, pady=5)
+
+tk.Label(frame, text="Password:").grid(row=3, column=0, padx=5, pady=5)
+entry_password = tk.Entry(frame, show="*")
+entry_password.grid(row=3, column=1, padx=5, pady=5)
+
+button_create_account = tk.Button(frame, text="Create Account", command=create_account)
+button_create_account.grid(row=4, column=0, pady=10)
+
+button_login = tk.Button(frame, text="Login", command=login)
+button_login.grid(row=4, column=1, pady=10)
+
+root.mainloop()
--- server.py
+++ server.py
@@ -1,36 +1,100 @@
 import socket
+import json
+import sqlite3
 import threading
 
+DATABASE_NAME = "accounts.db"
 
-HEADER = 1024
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
+def create_table():
+    conn = sqlite3.connect(DATABASE_NAME)
+    cursor = conn.cursor()
+
+    # Create a table to store accounts
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS accounts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT,
+            password TEXT
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+def account_exists(username):
+    conn = sqlite3.connect(DATABASE_NAME)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM accounts WHERE username=?", (username,))
+    account = cursor.fetchone()
+    conn.close()
+
+    return account is not None
 
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+def handle_client(client_socket):
+    create_table()
 
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION ] {addr} connected.")
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg=='0':
-                connected = False
-                print(f"[{addr}] diconnect")
+    data = client_socket.recv(1024).decode("utf-8")
+    request = json.loads(data)
 
-def start():
-    server.listen()
-    print(f"[LISTENING] server is listening on {SERVER}")
+    action = request.get("action")
+    username = request.get("username")
+    password = request.get("password")
+
+    if action == "create_account":
+        conn = None  # Initialize conn here
+
+        try:
+            if account_exists(username):
+                response = {"status": "Username already exists"}
+            else:
+                conn = sqlite3.connect(DATABASE_NAME)
+                cursor = conn.cursor()
+                cursor.execute('INSERT INTO accounts (username, password) VALUES (?, ?)', (username, password))
+                conn.commit()
+                response = {"status": "Account created successfully"}
+        except sqlite3.Error as e:
+            response = {"status": f"Error creating account: {str(e)}"}
+        finally:
+            if conn:
+                conn.close()
+
+    elif action == "login":
+        try:
+            conn = sqlite3.connect(DATABASE_NAME)
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM accounts WHERE username=? AND password=?', (username, password))
+            account = cursor.fetchone()
+
+            if account:
+                response = {"status": "Login successful"}
+            else:
+                response = {"status": "Invalid username or password"}
+        except sqlite3.Error as e:
+            response = {"status": f"Error during login: {str(e)}"}
+        finally:
+            conn.close()
+
+    else:
+        response = {"status": "Invalid action"}
+
+    client_socket.send(json.dumps(response).encode("utf-8"))
+    client_socket.close()
+
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("127.0.0.1", 5000))
+    server_socket.listen(5)
+
+    print("Server listening on port 5000")
+
     while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn,addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTION]] {threading.active_count() - 1}")
+        client_socket, addr = server_socket.accept()
+        print(f"Accepted connection from {addr}")
 
-start()
+        # Create a new thread for each client
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
+
+if __name__ == "__main__":
+    start_server()

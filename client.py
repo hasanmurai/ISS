09e68e5f631a13ed1d2ba1cd33f3a5dfebe888f9
Your Name <you@example.com>
2023-12-24 15:54:36 +0300

    stage 2.

--- client.py
+++ client.py
@@ -1,121 +1,277 @@
+import hashlib
 import tkinter as tk
-from tkinter import W, Checkbutton, IntVar, Radiobutton, messagebox
+from tkinter import W, Radiobutton, messagebox
 import socket
 import json
-
-def create_account_send_request(action, username, password,phone_number, address, national_number,role):
-    request = {"action": action, "username": username, "password": password, "phone_number": phone_number,
-                "address": address, "national_number": national_number, "role": role}
-    return json.dumps(request).encode("utf-8")
-def login_send_request(action, username, password):
-    request = {"action": action, "username": username, "password": password}
-    return json.dumps(request).encode("utf-8")
+from cryptography.fernet import Fernet
 
 
-def create_account():
+def symmetric_encryption(data, key):
+    fernet = Fernet(key.encode())
+    encrypted_data = {}
+
+    for field, value in data.items():
+        encrypted_data[field] = fernet.encrypt(value.encode()).decode()
+
+    return encrypted_data
+
+
+def symmetric_decryption(encrypted_data, key):
+    fernet = Fernet(key)
+    decrypted_data = {}
+
+    for field, value in encrypted_data.items():
+        decrypted_data[field] = fernet.decrypt(value)  
+        
+    return decrypted_data
+
+
+def hash(password):
+    hash = hashlib.sha256()
+    hash.update(password.encode() if isinstance(password, str) else password)
+    hash_value = hash.hexdigest()
+    return hash_value
+
+
+def copy_to_clipboard(message):
+    root.clipboard_clear()
+    root.clipboard_append(message)
+    root.update()
+
+
+def create_account(entry_ip, entry_port, entry_username, entry_password):
     try:
         server_ip = entry_ip.get()
         server_port = int(entry_port.get())
         username = entry_username.get()
-        password = entry_password.get()
-        phone_number = int(entry_phone_number.get())
-        address = entry_address.get()
-        national_number = int(entry_national_number.get())
-        role = int(entry_role.get())
-
+        password = hash(entry_password.get())
 
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect((server_ip, server_port))
 
         action = "create_account"
-        request_data = create_account_send_request(action, username, password,phone_number,address,national_number,role)
-        client_socket.send(request_data)
+        request_data = {"action": action, "username": username, "password": password}
+        client_socket.send(json.dumps(request_data).encode())
 
-        response_data = client_socket.recv(1024).decode("utf-8")
+        response_data = client_socket.recv(1024)
         response = json.loads(response_data)
 
-        messagebox.showinfo("Server Response", response['status'])
+        messagebox.showinfo("Server Response", response['message'])
+
+        # Display the encryption key in a custom dialog
+        if response['encryption_key']:
+            show_encryption_key(response['encryption_key'])
+            Complete_Information_Frame(client_socket)
+
 
     except socket.error as e:
         messagebox.showerror("Connection Error", "No server available")
     except Exception as e:
         messagebox.showerror("Error", f"An error occurred: {e}")
+    # finally:
+    #     client_socket.close()
 
-    finally:
-        client_socket.close()
 
-def login():
+
+
+def login(entry_ip, entry_port, entry_username, entry_password):
     try:
         server_ip = entry_ip.get()
         server_port = int(entry_port.get())
         username = entry_username.get()
-        password = entry_password.get()
+        password = hash(entry_password.get())
 
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect((server_ip, server_port))
 
         action = "login"
-        request_data = login_send_request(action, username, password)
-        client_socket.send(request_data)
+        request_data = {"action": action, "username": username, "password": password}
+        client_socket.send(json.dumps(request_data).encode())
 
-        response_data = client_socket.recv(1024).decode("utf-8")
+        response_data = client_socket.recv(1024).decode()
         response = json.loads(response_data)
-
-        messagebox.showinfo("Server Response", response['status'])
+        
+        messagebox.showinfo("Server Response", response['message'])
+        if response["status"]==200:
+            Student_or_Professor(response['role'], "main_frame")
+        else:
+            client_socket.close()
 
     except socket.error as e:
         messagebox.showerror("Connection Error", "No server available")
     except Exception as e:
         messagebox.showerror("Error", f"An error occurred: {e}")
 
-    finally:
-        client_socket.close()
+    # finally:
+    #     client_socket.close()
 
-# GUI setup
+
+def complete_information(_client_socket_, entry_phone_number,
+                         entry_national_number, entry_address, entry_role, entry_key):
+    try:
+        
+        client_socket = _client_socket_
+
+        phone_number = entry_phone_number.get()
+        national_number = entry_national_number.get()
+        address = entry_address.get()
+        role = entry_role.get()
+        key = entry_key.get()
+        request_data = {"phone_number": phone_number, "national_number": national_number,
+                        "address": address, "role": role}
+   
+        encrypted_data = symmetric_encryption(request_data, key)
+      
+
+        # client_socket.send(json.dumps(encrypted_data).encode())
+        # response_json = json.dumps(encrypted_data, default=lambda x: str(x))
+        client_socket.send(json.dumps(encrypted_data).encode())
+
+        response_data = client_socket.recv(1024).decode()
+        response = json.loads(response_data)
+        response = symmetric_decryption(response,key)
+        messagebox.showinfo("Server Response", response['message'])
+        Student_or_Professor(response["role"], "complete_information_frame")
+
+    except socket.error as e:
+        messagebox.showerror("Connection Error", "No server available")
+    except Exception as e:
+        messagebox.showerror("Error", f"An error has occurred: {e}")
+
+    # finally:
+    #     client_socket.close()
+
+
+def clear_widgets(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+
+def Student_or_Professor(role, frame):
+    if role == '1':
+        Student_Frame(frame)
+    else:
+        Professor_Frame(frame)
+
+
+def Main_Frame():
+    main_frame.tkraise()
+    main_frame.pack_propagate(False)
+
+    tk.Label(main_frame, text="Server IP:").grid(row=0, column=0, padx=5, pady=5)
+    entry_ip = tk.Entry(main_frame)
+    entry_ip.grid(row=0, column=1, padx=5, pady=5)
+
+    tk.Label(main_frame, text="Server Port:").grid(row=1, column=0, padx=5, pady=5)
+    entry_port = tk.Entry(main_frame)
+    entry_port.grid(row=1, column=1, padx=5, pady=5)
+
+    tk.Label(main_frame, text="Username:").grid(row=2, column=0, padx=5, pady=5)
+    entry_username = tk.Entry(main_frame)
+    entry_username.grid(row=2, column=1, padx=5, pady=5)
+
+    tk.Label(main_frame, text="Password:").grid(row=3, column=0, padx=5, pady=5)
+    entry_password = tk.Entry(main_frame, show="*")
+    entry_password.grid(row=3, column=1, padx=5, pady=5)
+
+    button_login = tk.Button(main_frame, text="Create Account",
+                             command=lambda: create_account(entry_ip, entry_port, entry_username, entry_password))
+    button_login.grid(row=4, column=0, pady=10)
+
+    button_create_account = tk.Button(main_frame, text="Login",
+                                      command=lambda: login(entry_ip, entry_port, entry_username, entry_password))
+    button_create_account.grid(row=4, column=1, pady=10)
+
+
+def show_encryption_key(encryption_key):
+    dialog = tk.Toplevel(root)
+    dialog.title("Encryption Key")
+
+    label = tk.Label(dialog, text=f"Encryption Key: {encryption_key}")
+    label.pack(padx=20, pady=20)
+
+    copy_button = tk.Button(dialog, text="Copy to Clipboard", command=lambda: copy_to_clipboard(encryption_key))
+    copy_button.pack()
+
+
+def Complete_Information_Frame(client_socket):
+    clear_widgets(main_frame)
+    complete_information_frame.tkraise()
+    complete_information_frame.pack_propagate(False)
+
+    tk.Label(complete_information_frame, text="Phone Number:").grid(row=0, column=0, padx=5, pady=5)
+    entry_phone_number = tk.Entry(complete_information_frame)
+    entry_phone_number.grid(row=0, column=1, padx=5, pady=5)
+
+    tk.Label(complete_information_frame, text="Address:").grid(row=1, column=0, padx=5, pady=5)
+    entry_address = tk.Entry(complete_information_frame)
+    entry_address.grid(row=1, column=1, padx=5, pady=5)
+
+    tk.Label(complete_information_frame, text="National Number:").grid(row=2, column=0, padx=5, pady=5)
+    entry_national_number = tk.Entry(complete_information_frame)
+    entry_national_number.grid(row=2, column=1, padx=5, pady=5)
+
+    entry_role = tk.StringVar()
+
+    Radiobutton(complete_information_frame, text='Student',
+                variable=entry_role, value='1').grid(row=3, column=0, sticky=W)
+    Radiobutton(complete_information_frame, text='Professor',
+                variable=entry_role, value='2').grid(row=3, column=1, sticky=W)
+    entry_role.set('1')
+
+    tk.Label(complete_information_frame, text="Encryption Key:").grid(row=4, column=0, padx=5, pady=5)
+    entry_key = tk.Entry(complete_information_frame)
+    entry_key.grid(row=4, column=1, padx=5, pady=5)
+
+    button_continue = tk.Button(complete_information_frame, text="Continue",
+                                command=lambda: complete_information(client_socket, entry_phone_number,
+                                                                     entry_national_number, entry_address, entry_role,
+                                                                     entry_key))
+    button_continue.grid(row=5, column=1, pady=10)
+
+
+def Student_Frame(frame):
+    if frame == "main_frame":
+        clear_widgets(main_frame)
+    else:
+        clear_widgets(student_frame)
+    student_frame.tkraise()
+    student_frame.pack_propagate(False)
+
+    tk.Label(student_frame, text="asd Number:").grid(row=0, column=0, padx=5, pady=5)
+    entry_phone_number = tk.Entry(student_frame)
+    entry_phone_number.grid(row=0, column=1, padx=5, pady=5)
+
+
+def Professor_Frame(frame):
+    if frame == "main_frame":
+        clear_widgets(main_frame)
+    else:
+        clear_widgets(professor_frame)
+    professor_frame.tkraise()
+    professor_frame.pack_propagate(False)
+
+    tk.Label(professor_frame, text="Phone Number:").grid(row=0, column=0, padx=5, pady=5)
+    entry_phone_number = tk.Entry(professor_frame)
+    entry_phone_number.grid(row=0, column=1, padx=5, pady=5)
+
+
 root = tk.Tk()
-root.title("Client Interface")
+root.title("Welcome")
+root.eval("tk::PlaceWindow . center")
 
-frame = tk.Frame(root)
-frame.pack(padx=20, pady=20)
+# x = root.winfo_screenwidth() // 2
+# y = int(root.winfo_screenheight() * 0.1)
+# root.geometry('500x600+' + str(x) + '+' + str(y))
 
-tk.Label(frame, text="Server IP:").grid(row=0, column=0, padx=5, pady=5)
-entry_ip = tk.Entry(frame)
-entry_ip.grid(row=0, column=1, padx=5, pady=5)
+# create a frame widgets
+main_frame = tk.Frame(root, width=500, height=600, bg="#3d6466")
+complete_information_frame = tk.Frame(root, bg="#3d6466")
+student_frame = tk.Frame(root, bg="#3d6466")
+professor_frame = tk.Frame(root, bg="#3d6466")
 
-tk.Label(frame, text="Server Port:").grid(row=1, column=0, padx=5, pady=5)
-entry_port = tk.Entry(frame)
-entry_port.grid(row=1, column=1, padx=5, pady=5)
+for frame in (main_frame, complete_information_frame, student_frame, professor_frame):
+    frame.grid(row=0, column=0, sticky="nesw")
 
-tk.Label(frame, text="Username:").grid(row=2, column=0, padx=5, pady=5)
-entry_username = tk.Entry(frame)
-entry_username.grid(row=2, column=1, padx=5, pady=5)
-
-tk.Label(frame, text="Password:").grid(row=3, column=0, padx=5, pady=5)
-entry_password = tk.Entry(frame, show="*")
-entry_password.grid(row=3, column=1, padx=5, pady=5)
-
-tk.Label(frame, text="Phone Number:").grid(row=4, column=0, padx=5, pady=5)
-entry_phone_number = tk.Entry(frame)
-entry_phone_number.grid(row=4, column=1, padx=5, pady=5)
-
-tk.Label(frame, text="Address:").grid(row=5, column=0, padx=5, pady=5)
-entry_address = tk.Entry(frame)
-entry_address.grid(row=5, column=1, padx=5, pady=5)
-
-tk.Label(frame, text="National Number:").grid(row=6, column=0, padx=5, pady=5)
-entry_national_number = tk.Entry(frame)
-entry_national_number.grid(row=6, column=1, padx=5, pady=5)
-
-entry_role = IntVar()
-
-Radiobutton(frame, text='Student', variable=entry_role, value=1).grid(row=7, column=0, sticky=W)
-Radiobutton(frame, text='Professor', variable=entry_role, value=2).grid(row=7, column=1, sticky=W)
-entry_role.set(1)
-
-button_create_account = tk.Button(frame, text="Create Account", command=create_account)
-button_create_account.grid(row=8, column=0, pady=10)
-
-button_login = tk.Button(frame, text="Login", command=login)
-button_login.grid(row=8, column=1, pady=10)
-
+Main_Frame()
 root.mainloop()
--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import socket
 import json
 import sqlite3
 import threading
+from cryptography.fernet import Fernet
+
 
 DATABASE_NAME = "accounts.db"
 
@@ -9,7 +11,6 @@ def create_table():
     conn = sqlite3.connect(DATABASE_NAME)
     cursor = conn.cursor()
 
-    # Create a table to store accounts with additional information
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS accounts (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -18,13 +19,39 @@ def create_table():
             phone_number TEXT,
             address TEXT,
             national_number TEXT,                       
-            role TEXT
+            role TEXT,
+            key TEXT
 
         )
     ''')
 
     conn.commit()
     conn.close()
+
+
+def encryption_key():
+    key=Fernet.generate_key()
+    return key
+
+def symmetric_encryption(data, key):
+    fernet = Fernet(key)
+    encrypted_data = {}
+
+    for field, value in data.items():
+        encrypted_data[field] = fernet.encrypt(value.encode()).decode()
+
+    return encrypted_data
+
+
+def symmetric_decryption(encrypted_data, key):
+    fernet = Fernet(key)
+    decrypted_data = {}
+
+    for field, value in encrypted_data.items():
+        decrypted_data[field] = fernet.decrypt(value)  
+        
+    return decrypted_data
+
 
 def account_exists(username):
     conn = sqlite3.connect(DATABASE_NAME)
@@ -35,9 +62,93 @@ def account_exists(username):
 
     return account is not None
 
-def handle_client(client_socket):
-    create_table()
+def create_account(client_socket, username,password):
+    try:
+            conn = sqlite3.connect(DATABASE_NAME)
 
+            if account_exists(username):
+                response = {"message": "Username already exists","encryption_key":False}
+            else:
+                key=encryption_key()
+                cursor = conn.cursor()
+                cursor.execute('''
+                    INSERT INTO accounts (username, password, key)
+                    VALUES (?, ?, ?)
+                ''', (username, password, key))
+                conn.commit()
+                response = {"message": "Account created successfully","encryption_key": key.decode()}
+    except sqlite3.Error as e:
+            response = {"message": f"Error creating account: {str(e)}"}
+    finally:
+        conn.close()
+        response_json = json.dumps(response, default=lambda x: str(x))
+        client_socket.send(response_json.encode())
+        
+
+def complete_information(client_socket, username):
+        try:
+        
+            conn = sqlite3.connect(DATABASE_NAME)
+            cursor = conn.cursor()
+        # cursor.execute('SELECT * FROM accounts WHERE username=? AND address IS NULL', (username,))
+        # information = cursor.fetchone()
+
+        # if information:
+          
+            cursor.execute("SELECT key FROM accounts WHERE username = ?", (username,))
+            isKey= cursor.fetchone()
+        # if isKey:
+                
+            key= isKey[0]
+            request= client_socket.recv(1024).decode()
+            data=json.loads(request)
+            
+            decrypted_data= symmetric_decryption(data, key)
+            phone_number= decrypted_data.get("phone_number")
+            address= decrypted_data.get("address")
+            national_number= decrypted_data.get("national_number")
+            role= decrypted_data.get("role")
+                            
+            cursor.execute('''
+            UPDATE accounts SET phone_number = ?, address = ?, 
+                national_number = ?, role = ? WHERE username = ?
+            ''', (phone_number, address, national_number, role, username))
+            conn.commit()
+            response = {"message": "Information Updated", "role": role.decode()}
+            response = symmetric_encryption(response, key)
+
+            
+    # except sqlite3.Error as e:
+    #         response = {"message":  f"Error during completing information: {str(e)}"}
+        finally:
+            response_json = json.dumps(response, default=lambda x: str(x))
+            client_socket.send(response_json.encode())
+            conn.close()
+
+
+def login(client_socket, username, password):
+    try:
+        conn = sqlite3.connect(DATABASE_NAME)
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM accounts WHERE username=? AND password=?', (username, password))
+        account = cursor.fetchone()
+
+        if account:
+            response = {"message": "Login successful", "role": '1', "status":200}        
+        else:
+            response = {"message": "Invalid username or password", "status" :404}
+    except sqlite3.Error as e:
+        response = {"message": f"Error during login: {str(e)}"}
+    finally:
+        conn.close()
+        response_json = json.dumps(response, default=lambda x: str(x))
+        client_socket.send(response_json.encode())
+        conn.close()
+
+
+def handle_client(_client_socket_):
+    create_table()
+    client_socket= _client_socket_
     data = client_socket.recv(1024).decode("utf-8")
     request = json.loads(data)
     action = request.get("action")
@@ -45,53 +156,19 @@ def handle_client(client_socket):
     password = request.get("password")
 
     if action == "create_account":
-        conn = None
-        phone_number = request.get("phone_number")
-        address = request.get("address")
-        national_number = request.get("national_number")
-        role = request.get("role")
-
-
-        try:
-            if account_exists(username):
-                response = {"status": "Username already exists"}
-            else:
-                
-                conn = sqlite3.connect(DATABASE_NAME)
-                cursor = conn.cursor()
-                cursor.execute('''
-                    INSERT INTO accounts (username, password, phone_number, address, national_number,role)
-                    VALUES (?, ?, ?, ?, ?, ?)
-                ''', (username, password, phone_number, address, national_number,role))
-                conn.commit()
-                response = {"status": "Account created successfully"}
-        except sqlite3.Error as e:
-            response = {"status": f"Error creating account: {str(e)}"}
-        finally:
-            if conn:
-                conn.close()
+        create_account(client_socket, username, password)
+        complete_information(client_socket, username)
+        
 
     elif action == "login":
-        try:
-            conn = sqlite3.connect(DATABASE_NAME)
-            cursor = conn.cursor()
-            cursor.execute('SELECT * FROM accounts WHERE username=? AND password=?', (username, password))
-            account = cursor.fetchone()
-
-            if account:
-                response = {"status": "Login successful"}
-            else:
-                response = {"status": "Invalid username or password"}
-        except sqlite3.Error as e:
-            response = {"status": f"Error during login: {str(e)}"}
-        finally:
-            conn.close()
-
+        login(client_socket, username, password)
+        
     else:
-        response = {"status": "Invalid action"}
+        response = {"message": "Invalid action"}
+        client_socket.send(json.dumps(response).encode("utf-8"))
+        client_socket.close()
 
-    client_socket.send(json.dumps(response).encode("utf-8"))
-    client_socket.close()
+    
 
 def start_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
